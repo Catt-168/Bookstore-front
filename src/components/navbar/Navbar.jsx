6e03ce73b70bfb704b7ee0f49b9da1a81bebc837
Catt-168 <satpaingthu2000@gmail.com>
2025-03-09 16:20:19 +0630

    ref: search box disabled

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   return (
@@ -20,9 +20,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/"
-            className={({ isActive }) =>
-              isActive ? "text-custom-orange" : ""
-            }
+            className={({ isActive }) => (isActive ? "text-custom-orange" : "")}
           >
             Home
           </NavLink>
@@ -30,9 +28,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/books"
-            className={({ isActive }) =>
-              isActive ? "text-custom-orange" : ""
-            }
+            className={({ isActive }) => (isActive ? "text-custom-orange" : "")}
           >
             Books
           </NavLink>
@@ -40,9 +36,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/contact-us"
-            className={({ isActive }) =>
-              isActive ? "text-custom-orange" : ""
-            }
+            className={({ isActive }) => (isActive ? "text-custom-orange" : "")}
           >
             Contact us
           </NavLink>
@@ -50,9 +44,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to="/about-us"
-            className={({ isActive }) =>
-             isActive ? "text-custom-orange" : ""
-            }
+            className={({ isActive }) => (isActive ? "text-custom-orange" : "")}
           >
             About us
           </NavLink>
@@ -63,17 +55,21 @@ const Navbar = () => {
       <div className="flex items-center relative">
         <MagnifyingGlassIcon className="h-6 w-6 text-gray-400 absolute left-3" />
         <input
+          disabled
           type="text"
           placeholder="Search"
-          className="w-lg pl-12 pr-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-custom-orange text-lg"
+          className="w-lg pl-12 pr-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-custom-orange text-lg disabled:bg-gray-200 disabled:text-gray-500 disabled:border-gray-300"
         />
       </div>
 
       {/* User Icon */}
       <NavLink to="/user-profile">
-      <img src="/user.png" alt="user" className="w-10 h-10 opacity-50 mr-20" />
+        <img
+          src="/user.png"
+          alt="user"
+          className="w-10 h-10 opacity-50 mr-20"
+        />
       </NavLink>
-      
     </nav>
   );
 };
